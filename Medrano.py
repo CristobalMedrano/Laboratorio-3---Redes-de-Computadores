e618b126deb95eef4e62b2343aa0cde9e8156ce3
Cristobal <cristobal.medrano@usach.cl>
2020-11-23 12:56:17 -0300

    AM done

--- Medrano.py
+++ Medrano.py
@@ -10,7 +10,7 @@ import scipy.signal as signal
 from scipy.io import wavfile
 from scipy.integrate import quad
 from scipy import interpolate
-from numpy import pi, cos, arange, fft, linspace
+from numpy import pi, cos, arange, fft, linspace, cumsum
 
 # CONSTANTS 
 # GLOBAL VARIABLES
@@ -132,13 +132,15 @@ def simulate_amplitude_modulation(k, m, fc, t):
     carrier_time, carrier_signal, carrier_sample_rate = am_carrier_signal(fc, t)
     
     # Plot the carrier signal
-    plot_signal(carrier_time, carrier_signal, "Carrier Signal in time", "Time(s)", "Amplitude", min_time, max_time)
+    plot_signal(carrier_time, carrier_signal, "AM Carrier Signal in time", "Time(s)", "Amplitude", min_time, max_time)
+    plot_signal(carrier_time, carrier_signal, "AM Carrier Signal in time (Zoom)", "Time(s)", "Amplitude", 0, 0.0010)
+
     
     # Get the Fourier transform of carrier signal
     carrier_signal_fft_freq, carrier_signal_fft = get_fourier_transform(carrier_sample_rate, carrier_signal)
     
     # Plot the Fourier transform of carrier fft signal
-    plot_signal(carrier_signal_fft_freq, abs(carrier_signal_fft), "Carrier Signal in Frequency", "Frequency(hz)", "|F(w)|", min(carrier_signal_fft_freq), max(carrier_signal_fft_freq))
+    plot_signal(carrier_signal_fft_freq, abs(carrier_signal_fft), "AM Carrier Signal in Frequency", "Frequency(hz)", "|F(w)|", min(carrier_signal_fft_freq), max(carrier_signal_fft_freq))
 
     # Interpolation of the original signal to the carrier frequency
     interpolated_signal = get_interpolated_signal(signal, fs)
@@ -150,15 +152,15 @@ def simulate_amplitude_modulation(k, m, fc, t):
     am_modulated_signal = interpolated_signal(carrier_time)*carrier_signal
 
     # Plot the modulated signal
-    plot_signal(carrier_time, am_modulated_signal, "Modulated Signal in time", "Time(s)", "Amplitude", min_time, max_time)
+    plot_signal(carrier_time, am_modulated_signal, "AM Modulated Signal in time", "Time(s)", "Amplitude", min_time, max_time)
     
     # Fourier transforma of modulated signal
     modulated_signal_fft_freq, modulated_signal_fft = get_fourier_transform(carrier_sample_rate, am_modulated_signal)
 
     # Plot the fourier transforma of modulated signal
-    plot_signal(modulated_signal_fft_freq, abs(modulated_signal_fft), "Modulated Signal in Frequency", "Frequency(hz)", "|F(w)|", min(modulated_signal_fft_freq), max(modulated_signal_fft_freq))
+    plot_signal(modulated_signal_fft_freq, abs(modulated_signal_fft), "AM Modulated Signal in Frequency", "Frequency(hz)", "|F(w)|", min(modulated_signal_fft_freq), max(modulated_signal_fft_freq))
     
-    return am_modulated_signal
+    return carrier_time, am_modulated_signal
 
 def simulate_amplitude_demodulation(k, m, fc, t):
     """ Get amplitude demodulation (AM) simulation
@@ -196,7 +198,7 @@ def simulate_amplitude_demodulation(k, m, fc, t):
     demodulated_signal_fft_freq, demodulated_signal_fft = get_fourier_transform(carrier_sample_rate, am_demodulated_signal)
     
     # Plot the fourier transform of demodulated signal
-    plot_signal(demodulated_signal_fft_freq, abs(demodulated_signal_fft), "Demodulated Signal in Frequency without low pass filter", "Frequency(hz)", "|F(w)|", min(demodulated_signal_fft_freq), max(demodulated_signal_fft_freq))
+    plot_signal(demodulated_signal_fft_freq, abs(demodulated_signal_fft), "AM Demodulated Signal in Frequency without low pass filter", "Frequency(hz)", "|F(w)|", min(demodulated_signal_fft_freq), max(demodulated_signal_fft_freq))
     
     # Apply low pass filter
     demodulated_signal_filtered = get_low_pass_filter(carrier_sample_rate, am_demodulated_signal, fc)
@@ -205,13 +207,13 @@ def simulate_amplitude_demodulation(k, m, fc, t):
     demodulated_signal_filtered_fft_freq, demodulated_signal_filtered_fft = get_fourier_transform(carrier_sample_rate, demodulated_signal_filtered)
 
     # Plot the fourier transform of demodulated signal with filter
-    plot_signal(demodulated_signal_filtered_fft_freq, abs(demodulated_signal_filtered_fft), "Demodulated Signal in Frequency with low pass filter", "Frequency(hz)", "|F(w)|", min(demodulated_signal_filtered_fft_freq), max(demodulated_signal_filtered_fft_freq))
-    plot_signal(demodulated_signal_filtered_fft_freq, abs(demodulated_signal_filtered_fft), "Demodulated Signal in Frequency with low pass filter (Zoom)", "Frequency(hz)", "|F(w)|", -fs/2, fs/2)
+    plot_signal(demodulated_signal_filtered_fft_freq, abs(demodulated_signal_filtered_fft), "AM Demodulated Signal in Frequency with low pass filter", "Frequency(hz)", "|F(w)|", min(demodulated_signal_filtered_fft_freq), max(demodulated_signal_filtered_fft_freq))
+    plot_signal(demodulated_signal_filtered_fft_freq, abs(demodulated_signal_filtered_fft), "AM Demodulated Signal in Frequency with low pass filter (Zoom)", "Frequency(hz)", "|F(w)|", -fs/2, fs/2)
       
     # Plot the demodulated signal
-    plot_signal(carrier_time, am_demodulated_signal, "Demodulated Signal in time", "Time(s)", "Amplitude", min_time, max_time)
+    plot_signal(carrier_time, am_demodulated_signal, "AM Demodulated Signal in time", "Time(s)", "Amplitude", min_time, max_time)
     
-    return am_demodulated_signal
+    return carrier_time, am_demodulated_signal
 
 def get_low_pass_filter(fs, wave, cut_fs):
     """ Obtain the wave filter by lowpass.
@@ -256,7 +258,33 @@ def simulate_frequency_modulation(k, m, fc, t):
     fm: ndarray
         Array of evenly spaced values with the time range modulation.
    """
-    return cos((2*pi*fc)*t + k*quad(m, a=0, b=t))
+    min_time = 0
+    max_time = t[-1]
+    
+    fs = m[0]
+    signal = m[1]
+
+    min_time = 0
+    max_time = t[-1]
+    
+    carrier_sample_rate = 4 * fc
+    carrier_time = arange(min_time, max_time, 1/carrier_sample_rate)
+    
+    interpolated_signal = get_interpolated_signal(signal, fs)
+    
+    sum_m = cumsum(interpolated_signal(carrier_time)) / fs
+    carrier_signal = cos(( 2 * pi * fc ) * carrier_time + sum_m)
+
+    
+    # Plot the carrier signal
+    plot_signal(carrier_time, carrier_signal, "FM Modulated Signal in time", "Time(s)", "Amplitude", min_time, max_time)
+    plot_signal(carrier_time, carrier_signal, "FM Modulated Signal in time (Zoom)", "Time(s)", "Amplitude", 1.3480, 1.3510)
+
+    # Get the Fourier transform of FM signal
+    carrier_signal_fft_freq, carrier_signal_fft = get_fourier_transform(carrier_sample_rate, carrier_signal)
+    
+    # Plot the Fourier transform of FM signal
+    plot_signal(carrier_signal_fft_freq, abs(carrier_signal_fft), "FM Modulated Signal in Frequency", "Frequency(hz)", "|F(w)|", min(carrier_signal_fft_freq), max(carrier_signal_fft_freq))
 
 def get_fourier_transform(fs, signal):
     """ Get the fourier transform of the audio signal.
@@ -326,18 +354,15 @@ def get_interpolated_signal(signal, fs):
 # MAIN
 def main():
     """ Main function of program """
-    filename = "handel.wav"
-    #filename = input("Choose an audio file (.wav) to read: ")
+    #filename = "handel.wav"
     print("\nLaboratorio 3 - Modulación Analógica\n(Desarrollado por: Cristóbal Medrano A.)\n")
     print("Adjunto a este programa, se encuentran 1 audio de prueba:")
     print("'handel.wav'\n")
-    #filename = input("Escriba el nombre del archivo de audio a procesar: ")
+    #filename = input("Choose an audio file (.wav) to read: ")
+    filename = input("Escriba el nombre del archivo de audio a procesar: ")
     print()
     if is_valid_audio_file(filename):
-        # Params
-        # fs, ts, fc, tc, fm
-
-         # Reading the audio file
+        # Reading the audio file
         fs, signal = wavfile.read(filename)
         t = get_time_audio_signal(fs, signal)
         
@@ -352,74 +377,47 @@ def main():
         
         # AM Modulation
         k = 1
-        fc = 14000 # hz
+        fc = 24000 # hz
         m = (fs, signal)
-        am_modulated_signal = simulate_amplitude_modulation(k, m, fc, t)
+        am_modulated_signal_time, am_modulated_signal = simulate_amplitude_modulation(k, m, fc, t)
+
 
         # AM Demodulacion
         m = (fs, am_modulated_signal)
-        simulate_amplitude_demodulation(k, m, fc, t)
+        am_demodulated_signal_time, am_demodulated_signal = simulate_amplitude_demodulation(k, m, fc, t)
 
+        # Plot Signal Comparation
+        plt.figure("Original and AM Modulated Signal")
+        plt.title("Original and AM Modulated Signal")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude")
+        plt.plot(am_modulated_signal_time, am_modulated_signal, label="AM Modulated Signal")
+        plt.plot(t, signal, label="Original Signal")
+        plt.xlim(2.4537, 2.4562)
+        plt.ylim(-3000, 3000)
+        plt.legend()
+        plt.grid()
+
+        # Plot modulation and demodulation comparation
+        plt.figure("Comparation Original and AM Demodulated Signal")
+        plt.title("Comparation Original and AM Demodulated Signal")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude")
+        plt.plot(am_demodulated_signal_time, am_demodulated_signal, label="AM Demodulated Signal")
+        plt.plot(t, signal, label="Original Signal")
+        plt.xlim(2.4537, 2.4562)
+        plt.ylim(-3000, 3000)
+        plt.legend()
+        plt.grid()
+
+        # FM Modultation
+        k = 1
+        fc = 24000 # hz
+        m = (fs, signal)
+        simulate_frequency_modulation(k, m, fc, t)
         plt.show()
-        # Transformada de Fourier de la señal Demodulada
-        # Aplicacion del filtro pasa bajos
-        # Transformada de Fourier de la señal demodulada luego de filtro
 
-        '''# frecuencia de muestreo de la portadora
-        carrier_sample_rate = 4*fc #3000 hz
-        carrier_time = arange(0, t[-1], 1/carrier_sample_rate)
-        carrier_signal = am_carrier_signal(fc, carrier_time)
-        plot_signal(carrier_time, carrier_signal, "Carrier Signal in time", "Time(s)", "Amplitude", 0, t[-1])
-        
-        carrier_signal_fft_freq, carrier_signal_fft = get_fourier_transform(carrier_sample_rate, carrier_signal)
-        plot_signal(carrier_signal_fft_freq, abs(carrier_signal_fft), "Carrier Signal in Frequency", "Frequency(hz)", "|F(w)|", min(carrier_signal_fft_freq), max(carrier_signal_fft_freq))
-
-        print('Señal Portadora (Frecuencia [fc]): ', fc, 'hz.')
-        print('Señal Portadora (Frecuencia de Muestreo [carrier_sample_rate]): ', len(carrier_time), 'hz.')
-        #plt.show()
-
-        # Interpolacion de la señal original a la frecuencia de la portadora
-        interpolated_signal = get_interpolated_signal(signal, fs)
-
-        print("largo de la señal "+ str(len(interpolated_signal(carrier_time))))
-        print("largo de la señal portadora "+ str(len(carrier_signal)))
-        plot_signal(carrier_time, interpolated_signal(carrier_time), "Interpolated Signal in time", "Time(s)", "Amplitude", 0, t[-1])
-
-
-        am_modulated_signal = interpolated_signal(carrier_time)*carrier_signal
-        plot_signal(carrier_time, am_modulated_signal, "Modulated Signal in time", "Time(s)", "Amplitude", 0, t[-1])
-        plt.show()
-        '''
-        
-
-        # Modulacion FM
-        # Grafico en el tiempo FM
-        # Transformada de Fourier FM
-
-        #A partir de lo anterior calcular la fc y tc de la portadora
-        #Grafico y transformada
-        
-        #fc = fs/4 # hz
-
-        #plot_signal(t, am_carrier_signal(fc, t), "Carrier Signal in time", "time (s)", "amplitude", 0, 0.01)
-
-        #am_carrier_fft_freq, am_carrier_fft_signal = get_fourier_transform(fc, am_carrier_signal(fc, t))
-        #plot_signal(am_carrier_fft_freq, abs(am_carrier_fft_signal), "Carrier Signal in frequency", 'Frequency (Hz)', '|F(w)|', -fc/2, fc/2)
-        #plt.show()
-        #f_carrier = am_carrier_signal(fc, t)
-        #f_carrier_wave_fft = abs(fft.fft(f_carrier))
-        #f_carrier_fs_fft = fft.fftfreq(f_carrier_wave_fft.size) 
-        #plot_signal(f_carrier_fs_fft, f_carrier_wave_fft, "Carrier Signal in frequency", "frequency (hz)", "|f(w)|", min(f_carrier_fs_fft), max(f_carrier_fs_fft))
-        #plt.show()
-        #plot_signal(time, am_carrier_signal(fs, time), "Signal in time carrier", "time (s)", "amplitude", min(time), max(time))
-        #print(am)
-        #print(fs)
-        #print(signal)
-        #print(t)
     else: 
         print("El archivo ingresado no existe.")
 
 main()
-#REFERENCES
-#https://medium.com/@nabanita.sarkar/simulating-amplitude-modulation-using-python-6ed03eb4e712
-#https://uvirtual.usach.cl/moodle/pluginfile.php/312438/mod_resource/content/0/Laboratorio%203%20-%20Modulaci%C3%B3n%20Anal%C3%B3gica.pdf
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-Laboratorio-3---Redes-de-Computadores
+# Laboratorio 3 - Redes de Computadores
+
+Este laboratorio tiene como función realizar la modulación analógica.
+
+# Requerimientos
+
+Los requerimientos necesarios de software para ejecutar el programa son:
+- Python 3.8.3
+- Instalar en Python las siguientes bibliotecas:
+    - La biblioteca numpy
+    - La biblioteca matplotlib
+    - La biblioteca time
+    - La libreria Os-Path (Incluida en la instalación de Python 3.8.3)
+
+
+## Instalación
+
+Instalar [Python](https://www.python.org/downloads/) en su versión 3.8.3 y
+Usar el manejador de paquetes [pip](https://pip.pypa.io/en/stable/) para instalar:
+
+```bash
+pip install numpy
+pip install matplotlib
+pip install time
+```
+
+## Uso
+Para utilizar este programa, simplemente debe ejecutar el archivo "Medrano.py", para ello, en su terminal preferido, debe escribir:
+
+```bash
+python Medrano.py
+```
+
+**Es importante que el comando sea escrito en el mismo directorio donde se encuentra el archivo .py**
+Luego debe seguir las intrucciones que se le indican por pantalla.
+
+![Ejecución](https://i.imgur.com/dBqGbZn.png)
+
+## Licencia
+Copyright (C) 2020 - Redes de Computadores - Laboratorio 3 - Cristóbal Medrano
